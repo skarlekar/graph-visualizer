5d7bd933668ffdc46eb302abbab1b9655d1acf93
Diego Figueroa <diegofigueroa79@gmail.com>
2024-05-21 12:44:49 -0700

    Update opencypher_neptune_qa.py

--- src/opencypher_neptune_qa.py
+++ src/opencypher_neptune_qa.py
@@ -17,6 +17,39 @@ bedrock_runtime = boto3.client("bedrock-runtime", region_name='us-east-1')
 modelId = "anthropic.claude-3-sonnet-20240229-v1:0"
 llm = BedrockChat(model_id=modelId, client=bedrock_runtime, model_kwargs={"temperature": 0})
 
+opencypher_examples = """
+<Examples>
+<question>
+Who is the property inspector for Sample Gardens?
+</question>
+
+MATCH (p:Property {hasPropertyName: 'Sample Gardens'})
+RETURN p.hasPropertyInspector AS property_inspector
+
+<question>
+What is the address for Sample Gardens?
+</question>
+
+MATCH (p:Property {hasPropertyName: 'Sample Gardens'})-[:hasPropertyAddress]-> (d)
+RETURN d AS property_address
+
+<question>
+Does Sample Gardens have Green Building Certfication?
+</question>
+
+MATCH (p:Property {hasPropertyName: 'Sample Gardens'})
+RETURN p.hasGreenBuildingCertification AS green_building_certification
+
+<question>
+Who is the lender for Sample Gardens?
+</question>
+
+MATCH (p:Property {hasPropertyName: 'Sample Gardens'})-[:hasLender]-> (d)
+RETURN d AS lender
+
+</Examples>
+"""
+
 chain = NeptuneOpenCypherQAChain.from_llm(llm=llm, graph=graph, verbose=True)
 
 chain.invoke("How many units does Sample Gardens property have?")

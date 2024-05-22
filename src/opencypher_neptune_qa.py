91159937c3de1374f9bffb54593309dae830e44b
Diego Figueroa <diegofigueroa79@gmail.com>
2024-05-22 13:46:07 -0700

    Update opencypher_neptune_qa.py

--- src/opencypher_neptune_qa.py
+++ src/opencypher_neptune_qa.py
@@ -65,6 +65,13 @@ Does Happy Apartments qualify for a green loan?
 MATCH (p:Property {hasPropertyName: 'Happy Apartments'})
 RETURN p.hasGreenBuildingCertification AS greenBuildingCertification, p.hasGreenBuildingCertificationAgency AS greenBuildingCertificationAgency, p.hasGreenBuildingUpgrades AS greenBuildingUpgrades, p.annualEnergyConsumptionReductionCommitment AS annualEnergyConsumptionReductionCommitment, p.annualWaterConsumptionReductionCommitment AS annualWaterConsumptionReductionCommitment 
 
+<question>
+Have we seen Happy Apartments property before? If yes, what was the approval decision?
+</question>
+
+MATCH (p:Property {hasPropertyName: 'Happy Apartments'})
+WHERE exists(p.hasApprovalDecision)
+RETURN p.hasApprovalDecision, p.hasApprovalDate, p.hasApprovalNotes, p.hasAssetManagementNotes
 </Examples>
 """
 
@@ -76,7 +83,7 @@ cypher_qa_mod_template = CYPHER_QA_TEMPLATE.replace("Here is an example",prompt_
 qa_prompt = PromptTemplate(input_variables=["context","question"],template=cypher_qa_mod_template)
 chain = NeptuneOpenCypherQAChain.from_llm(llm=llm, graph=graph, verbose=True, qa_prompt=qa_prompt,extra_instructions=opencypher_examples)
 
-questions = ["Does Sample Gardens advance housing goals?","Does Sample Gardens qualify for a green loan?"]
+questions = ["Have we seen this property before? If yes, what was the loan approval decision?", "Does Sample Gardens advance housing goals?","Does Sample Gardens qualify for a green loan?"]
 for q in questions:
     print(q)
     chain_response=chain.invoke(q)

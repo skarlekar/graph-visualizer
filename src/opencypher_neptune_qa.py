28e2492172d204dfd06cd1b6bdc6fd509f047d06
Diego Figueroa <diegofigueroa79@gmail.com>
2024-05-22 14:55:07 -0700

    Update opencypher_neptune_qa.py

--- src/opencypher_neptune_qa.py
+++ src/opencypher_neptune_qa.py
@@ -22,6 +22,8 @@ modelId = "anthropic.claude-3-sonnet-20240229-v1:0"
 llm = BedrockChat(model_id=modelId, client=bedrock_runtime, model_kwargs={"temperature": 0})
 
 opencypher_examples = """
+<examples>
+
 <question>
 Does Sample Gardens qualify for a green loan?
 </question>
@@ -78,7 +80,8 @@ Have we seen Happy Apartments property before? If yes, what was the approval dec
 MATCH (p:Property {hasPropertyName: 'Happy Apartments'})-[:hasPropertyAddress]-> (a)
 WHERE exists(p.hasApprovalDecision)
 RETURN a.hasCity AS city, a.hasStreetAddress AS streetAddress, a.hasState AS state, a.hasZipCode AS zipCode, p.hasPropertyName AS propertyName, p.hasApprovalDecision AS approvalDecision, p:hasApprovalNotes AS approvalNotes, p:hasAssetManagementNotes AS assetManagementNotes
-</Examples>
+
+</examples>
 """
 
 prompt_dir = os.path.abspath(os.path.join(os.getcwd(),"../","prompts"))

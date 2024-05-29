bcbf5aedce7ee1f2ee23effc026c58c2ecde004e
skybrodowski <46693094+skybrodowski@users.noreply.github.com>
2024-05-29 19:39:25 +0000

    Adding invoking the vertices prompt

--- master/Knowledge_Base/src/rdbms-to-gremlin-csv.py
+++ master/Knowledge_Base/src/rdbms-to-gremlin-csv.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import boto3
+from langchain_community.chat_models import BedrockChat
+import json
 
 def connect_to_bedrock():
     boto_session = boto3.Session()
@@ -12,8 +14,17 @@ def connect_to_bedrock():
     return llm
 
 def main():
+    table_locations={
+            "Loan":"../data/structured-data/Loan-data.csv",
+            "Property":"../data/structured-data/Property-data.csv",
+            "Underwriting":"../data/structured-data/Underwriting-data.csv",
+            "Inspection":"../data/structured-data/Inspection-data.csv"
+    }
     llm = connect_to_bedrock()
     vertices_prompt = open("../prompts/gremlin-vertices-headers-from-sql","r").read()
+    response = llm.invoke(input=vertices_prompt)
+    vertices_mapping = json.dumps(response.content)
+    print(vertices_mapping)
 
 if __name__ == '__main__':
     main()

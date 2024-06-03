155688ad9286f64caf214a2cb885fa17329dec11
skybrodowski <46693094+skybrodowski@users.noreply.github.com>
2024-06-03 20:54:16 +0000

    Testing query result

--- master/Knowledge_Base/src/rdf-to-cypher-neptune.py
+++ master/Knowledge_Base/src/rdf-to-cypher-neptune.py
@@ -19,9 +19,9 @@ def connect_to_bedrock():
   boto_session = boto3.Session()
   aws_region = boto_session.region_name
   bedrock_runtime = boto_session.client("bedrock-runtime", region_name="us-east-1")
-  
+
   modelId = "anthropic.claude-3-sonnet-20240229-v1:0"
-  
+
   llm = BedrockChat(model_id=modelId, client=bedrock_runtime, model_kwargs={"temperature": 0})
   return llm
 
@@ -30,46 +30,46 @@ def get_cypher_query(rdf_data):
   environment=Environment(loader=FileSystemLoader(template_dir))
   template=environment.get_template("cypher-merge-rdf-template.txt")
   cypher_gen_prompt=template.render(rdf_data=rdf_data,neptune_schema=graph.get_schema)
-  print(cypher_gen_prompt)
 
   llm = connect_to_bedrock()
-  #response = llm.invoke(input=vertices_prompt)
-  #return response
+  response = llm.invoke(input=cypher_gen_prompt)
+  return response
 
 def execute_cypher_query(cypher_query):
   graph.query(cypher_query)
-  
+
 def main():
   rdf_data="""
   @prefix mf: <http://example.org/multifamily#> .
   @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
-  
+
   <mf:100RentalUnits> a mf:TotalRentalUnits .
-  
+
   <mf:101TotalUnits> a mf:TotalUnits .
-  
+
   <mf:1NonRevenueUnit> a mf:NonRevenueUnits .
-  
+
   <mf:ConventionalLoan> a mf:LoanType .
-  
+
   <mf:MidCityBank> a mf:Lender .
-  
+
   <mf:NSPDevelopmentPartners> a mf:OwnerEntity .
-  
+
   <mf:NSPManagementPartners> a mf:ManagementAgent .
-  
+
   <mf:OwnerAddress> a mf:OwnerAddress ;
       mf:hasCity "Mid City" ;
       mf:hasState "Ohio" ;
       mf:hasStreetAddress "600 Main Street" ;
       mf:hasZipCode "23445" .
-  
+
   <mf:SampleGardens> a mf:Property ;
       mf:annualEnergyConsumptionReductionCommitment "25%" ;
       mf:annualWaterConsumptionReductionCommitment "30%" ;
       mf:hasApprovalDate "01/04/2018" ;
       mf:hasApprovalDecision "yes" ;
-      mf:hasApprovalNotes "Based on the strength and experience of the sponsor including managing other seniors assets in the same market and state for many years as well as the financial improvement of the asset, this supplemental transaction is recommended for Loan Committee Approval." ;
+      mf:hasApprovalNotes "Based on the strength and experience of the sponsor including managing other seniors assets in the same market and state for many years as well as the financial improvement of the as
+set, this supplemental transaction is recommended for Loan Committee Approval." ;
       mf:hasAssetManagementNotes "The property is maintained well and the rentals have met projected expectations." ;
       mf:hasAssetManagementNotesDate "01/05/2021" ;
       mf:hasGreenBuildingCertification "yes" ;
@@ -95,7 +95,7 @@ def main():
       mf:hasTotalRentalUnits mf:100RentalUnits ;
       mf:hasTotalUnits mf:101TotalUnits ;
       mf:hasUnits "101"^^xsd:int .
-  
+
   <mf:SampleGardensAddress> a mf:PropertyAddress ;
       mf:hasCity "Mid City" ;
       mf:hasLowIncomeUnits "20" ;
@@ -105,19 +105,19 @@ def main():
       mf:hasVeryLowIncomeUnits "10" ;
       mf:hasZipCode "23445" ;
       mf:isSmallMultifamilyUnitMeetingLowIncome "yes" .
-  
+
   <mf:SusanDeveloper> a mf:OwnerContact ;
       mf:hasEmail "susan@NSPDP.com" ;
       mf:hasName "Susan Developer" ;
       mf:hasPhone "(215) 555-1212" .
-  
+
   <mf:WallyLendor> a mf:LenderContact ;
       mf:hasEmail "wlendor@MCB.com" ;
       mf:hasName "Wally Lendor" ;
       mf:hasPhone "(215) 555-1214" .
   """
   cypher_query = get_cypher_query(rdf_data)
-  print(cypher_query)
+  print(cypher_query.content)
   #execute_cypher_query(cypher_query)
 
 if __name__ == '__main__':

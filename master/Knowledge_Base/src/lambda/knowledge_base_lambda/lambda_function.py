7d97d93dcf5ea6f5125d204b6adb5b161fd6694d
Diego Figueroa <diegofigueroa79@gmail.com>
2024-06-04 07:41:58 -0700

    update lambda function

--- master/Knowledge_Base/src/lambda/knowledge_base_lambda/lambda_function.py
+++ master/Knowledge_Base/src/lambda/knowledge_base_lambda/lambda_function.py
@@ -1,5 +1,5 @@
-from rdf_extractor import extract, clean_up, upload_to_s3
-from rdf_to_cypher_neptune import main as upload_to_neptune
+from knowledge_base_lambda.rdf_extractor import extract
+from knowledge_base_lambda.rdf_to_cypher_neptune import main as upload_to_neptune
 import json
 
 
@@ -9,7 +9,7 @@ def lambda_handler(event, context):
     ontology_key = event['ontology'] + '.ttl'
     
     graph = extract(doc_link=document_key, ontology_link=ontology_key)
-    upload_to_neptune(rdf_graph=graph)
+    upload_to_neptune(rdf_data=graph.serialize(format='ttl'))
     
     return {
         'statusCode': 200,

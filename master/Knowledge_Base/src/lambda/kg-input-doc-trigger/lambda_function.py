8e56df91deed06f8377d8f1ea6b4027a0284054d
Diego Figueroa <diegofigueroa79@gmail.com>
2024-06-05 08:01:12 -0700

    upload kg-input-doc-trigger code

---
+++ master/Knowledge_Base/src/lambda/kg-input-doc-trigger/lambda_function.py
@@ -0,0 +1,38 @@
+import json
+from urllib import parse
+import boto3
+from botocore.config import Config
+
+
+def lambda_handler(event, context):
+    # TODO implement
+    s3 = boto3.client('s3', region_name='us-east-1')
+    # Get the bucket name, key, and metadata ontology value from the event
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    obj = s3.get_object(Bucket=bucket, Key=key)
+    ontology = obj['Metadata']['ontology']
+    
+    payload = {
+        'document': key,
+        'ontology': ontology
+    }
+    
+    lambda_config = Config(
+        read_timeout=900,
+        connect_timeout=900,
+        retries={"max_attempts": 0}
+    )
+    
+    client = boto3.client('lambda', region_name='us-east-1', config=lambda_config)
+    response = client.invoke(
+        FunctionName='kg-1',
+        InvocationType='RequestResponse',
+        Payload=json.dumps(payload)
+    )
+    print(json.load(response['Payload']))
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from kg-input-doc-trigger!')
+    }
